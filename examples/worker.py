a4ac659aefe5ba13effb2de0a16e4759e8163f1d
Michael Merickel <michael@merickel.org>
2025-01-06 17:26:27 -0700

    make the worker example a little more robust by adding a database connection to the job

--- examples/worker.py
+++ examples/worker.py
@@ -2,6 +2,8 @@ import argparse
 import logging
 from pprint import pprint
 import sqlalchemy as sa
+import sqlalchemy.orm
+import time
 
 from psycopg2_mq import MQWorker, make_default_model
 
@@ -10,18 +12,28 @@ model = make_default_model(metadata)
 
 
 class DummyQueue:
+    def __init__(self, engine):
+        self.dbmaker = sa.orm.sessionmaker(bind=engine)
+
     def execute_job(self, job):
-        result = {
-            'id': job.id,
-            'queue': job.queue,
-            'method': job.method,
-            'args': job.args,
-            'listener_ids': job.listener_ids,
-            'schedule_ids': job.schedule_ids,
-            'trace': job.trace,
-        }
-        pprint(result)
-        return result
+        db = self.dbmaker()
+        with db:
+            db.execute(sa.text('select 1')).all()
+            if job.method == 'sleep':
+                duration = job.args['duration']
+                print(f'sleeping {duration}')
+                time.sleep(duration)
+            result = {
+                'id': job.id,
+                'queue': job.queue,
+                'method': job.method,
+                'args': job.args,
+                'listener_ids': job.listener_ids,
+                'schedule_ids': job.schedule_ids,
+                'trace': job.trace,
+            }
+            pprint(result)
+            return result
 
 
 def main():
@@ -35,10 +47,11 @@ def main():
     metadata.create_all(engine)
 
     queues = {
-        'dummy': DummyQueue(),
+        'dummy': DummyQueue(engine),
     }
 
     worker = MQWorker(
+        threads=2,
         engine=engine,
         queues=queues,
         model=model,
